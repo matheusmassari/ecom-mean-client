1d035ff42afbeaea09bd1c028cdfe46416609f6e
Matheus Massari <massari.matheus@gmail.com>
2022-11-11 16:14:14 -0300

    Feat: login service setup

--- eshop/libs/users/src/lib/pages/login/login.component.ts
+++ eshop/libs/users/src/lib/pages/login/login.component.ts
@@ -1,5 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
     selector: 'users-login',
@@ -8,8 +10,11 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
     loginFormGroup: FormGroup;
+    isSubmitted = false;
+    authError = false;
+    authMessage = 'Invalid credentials.';
 
-    constructor(private formBuilder: FormBuilder) {}
+    constructor(private formBuilder: FormBuilder, private auth: AuthService) {}
 
     ngOnInit(): void {
         this._initForm();
@@ -20,5 +25,28 @@ export class LoginComponent implements OnInit {
             email: ['', [Validators.required, Validators.email]],
             password: ['', Validators.required]
         });
+    }
+
+    onSubmit() {
+        this.isSubmitted = true;
+        if (this.loginFormGroup.invalid) {
+            return;
+        }
+        this.auth.login(this.loginForm.email.value, this.loginForm.password.value).subscribe({
+            next: (user) => {
+                this.authError = false;
+                console.log(user);
+            },
+            error: (error: HttpErrorResponse) => {
+                this.authError = true;
+                if (error.status !== 400) {
+                    this.authMessage = 'Internal server error. Please try again later.';
+                }
+            }
+        });
+    }
+
+    get loginForm() {
+        return this.loginFormGroup.controls;
     }
 }
